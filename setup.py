c145011043c748aa1f071255445b1d6ee91d065d
Jeremy Ephron <jeremyephron@gmail.com>
2020-12-20 02:47:45 -0800

    new version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="simplegmail",
-    version="3.0.0",
+    version="3.1.0",
     url="https://github.com/jeremyephron/simple-gmail",
     author="Jeremy Ephron",
     author_email="jeremyephron@gmail.com",
