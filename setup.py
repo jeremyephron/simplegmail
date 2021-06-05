0ffc64866dba526e59da0b4b4c7e085bb14f39e0
Jeremy Ephron <jeremyephron@gmail.com>
2021-06-05 14:27:59 -0700

    case insensitive header check

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="simplegmail",
-    version="4.0.2",
+    version="4.0.3",
     url="https://github.com/jeremyephron/simplegmail",
     author="Jeremy Ephron",
     author_email="jeremyephron@gmail.com",
