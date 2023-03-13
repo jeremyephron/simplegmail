7e1dcb9d0f6214581095b38965325c3ba08f3543
Jeremy Ephron <jeremye@cs.stanford.edu>
2023-03-13 03:29:49 -0700

    Bump version to 4.1.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="simplegmail",
-    version="4.1.0",
+    version="4.1.1",
     url="https://github.com/jeremyephron/simplegmail",
     author="Jeremy Ephron",
     author_email="jeremye@cs.stanford.edu",
